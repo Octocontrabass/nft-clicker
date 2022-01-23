c1b719b894f6460fae0f677fcc583b4546f5b19c
Octocontrabass <joe.derpa@gmail.com>
2022-01-22 19:48:58 -0800

    Grab JSON too
    Also makes it faster?

--- bayc.sh
+++ bayc.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
-for i in `seq 0 9999`
+#!/bin/bash
+curl --retry-all-errors --retry 255 --create-dirs "https://ipfs.io/ipfs/QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/[0-9999]" -o "bayc/#1.json"
+
+for i in {0..9999}
 do
-    sem -j10 curl -s `curl -s https://ipfs.io/ipfs/QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/$i | jq -r .image | sed 's,ipfs://,https://ipfs.io/ipfs/,g'` > bayc-$i.png
+    curl --retry-all-errors --retry 255 --create-dirs `jq -r .image bayc/$i.json | sed 's,ipfs://,https://ipfs.io/ipfs/,g'` -o "bayc/$i.png"
 done
-sem --wait
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # NFT Clicker
 
 Convenient scripts to automate right-clicking on NFTs.
+
+To do: the IPFS gateway likes to return errors in a way curl doesn't catch, so some right-clicks silently fail.
